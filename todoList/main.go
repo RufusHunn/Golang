028401087687b6a79b65422c8951b524c671ed85
ruth.hunn <ruth.hunn@specsavers.com>
2025-06-19 09:41:41 +0100

    trying to write multiple lines

--- todoList/main.go
+++ todoList/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 // Run below like:
 // go build main.go
-// ./main.exe --item=todo --status=completed
+// ./main.exe --item='todo list' --status=completed
 
 func main() {
+
 	item := flag.String("item", "activity", "Your activity")
 	status := flag.String("status", "not completed", "Item status")
 
@@ -17,4 +26,28 @@ func main() {
 
 	fmt.Println("Item: ", *item)
 	fmt.Println("Status: ", *status)
+
+	f, err := os.Open("tmp/dat1")
+	check(err)
+	defer f.Close()
+
+	b1 := make([]byte, 32)
+	m1, err := f.Read(b1)
+	check(err)
+	readFileAsString := string(b1)
+
+	fmt.Println("Existing lines: ", readFileAsString, " with len ", m1)
+
+	lines := []string{readFileAsString, *item}
+	output := strings.Join(lines, "\n")
+	d1 := []byte(output)
+	fmt.Println("Preparing to write lines: ", string(d1))
+
+	// erx := os.WriteFile("tmp/dat1", d1, 0644) // This does not actually write - why?
+	erx := os.WriteFile("tmp/dat1", []byte(*item), 0644) // Writes latest input but we want all lines!
+	check(erx)
+	fmt.Println("wrote lines: ", d1)
+
+	f.Sync()
+
 }
